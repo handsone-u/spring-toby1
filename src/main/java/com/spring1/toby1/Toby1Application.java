a57f4c5731ffe67eda9881db637485a33b0af3a1
handsone-u <jk04172@naver.com>
2021-10-27 21:19:47 +0900

    Initial commit

---
+++ src/main/java/com/spring1/toby1/Toby1Application.java
@@ -0,0 +1,13 @@
+package com.spring1.toby1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toby1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toby1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring1/toby1/Toby1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring1.toby1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toby1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
