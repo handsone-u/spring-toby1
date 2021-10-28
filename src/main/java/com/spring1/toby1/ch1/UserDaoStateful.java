631ca76bc95e5d9092fab51e6038755caf16f9e8
handsone-u <jk04172@naver.com>
2021-10-28 16:34:00 +0900

    Singleton - Stateful

---
+++ src/main/java/com/spring1/toby1/ch1/UserDaoStateful.java
@@ -0,0 +1,13 @@
+package com.spring1.toby1.ch1;
+
+import java.sql.Connection;
+
+public class UserDaoStateful {
+    private ConnectionMaker connectionMaker;
+    private Connection connection;
+    private User1 user;
+
+    public User1 get(String id) {
+        return null;
+    }
+}
